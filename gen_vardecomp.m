0a02537394c28b4c1fdceecfdb5dedb4f7815576
lauragati <lauraember@gmail.com>
2017-10-03 14:27:53 -0400

    Oct 3 2017: variance decomposition, looked at block bootstrap (not yet working)

---
+++ gen_vardecomp.m
@@ -0,0 +1,34 @@
+function [vardec] = gen_vardecomp(IRFs,m,H)
+% Perform variance decomposition after estimating a VAR
+% m = the horizon of the variance decomposition (can equal, but not exceed H)
+% H = the number of IRs generated in plotIRFs.m
+
+% Marco Brianti, Laura Gati    Oct 3, 2017
+
+if m > H
+    error('number of periods for variance decomposition cannot exceed the number of IRs generated')
+end
+
+nvar = size(IRFs,1);
+nshocks = nvar;
+% cut out those IRs we don't need
+IRFs = IRFs(:,1:m,:);
+
+% Generate denominator
+den = sum(sum(IRFs.^2,3),2); % one value for each variable, so (nvar x 1)
+
+% Generate numerator
+num = sum(IRFs.^2,2);
+num = squeeze(num); % get rid of unnecessary dimension
+% one value for each variable AND each shock, so (nvar x nshocks)
+
+% Generate variance decomposition table
+vardec = zeros(nvar, nshocks);
+for i_var = 1:nvar
+    vardec(i_var,:) = num(i_var,:)/den(i_var);
+end
+
+% check if they sum up to 1
+if sum(vardec,2) ~= ones(nvar,1)
+    error('Variance decomposition doesn''t sum to 1')
+end
