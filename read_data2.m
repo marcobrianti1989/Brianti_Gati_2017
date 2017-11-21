34ebca19b5179435a2d62a01c9b8688f3669e9fb
lauragati <lauraember@gmail.com>
2017-11-21 09:54:15 -0500

    Edited read_data2.m
    1.) Added position, so it automatically spits out the position of the
    variables and arranges the variables in the order you specify in the
    excel file.

--- read_data2.m
+++ read_data2.m
@@ -1,4 +1,16 @@
 function [data, var_names] = read_data2(filename, sheet, range)
+% This file is completely general function for reading in data. The only
+% requirement for its use is that the dataset in Excel is structured in a
+% specific way, which is:
+% In one of the sheets, which you specify here using the sheet argument of
+% this function, you should have the following rows:
+% Row 1: Name of the variable (shorthand)
+% Row 2: Transformation that is to be done on the variable (see possible transformations below)
+% Row 3: In or Out: 1 if the series is to be used in the dataset (in a VAR
+% maybe), 0 otherwise
+% Row 4: No. series = just an accounting of how many series we have
+% Row 5: Position: the position of the series in the dataset to be used (could play a role for Cholesky VARs for ex.)
+
 
 % Create the correct path 
 base_path = pwd;
@@ -14,7 +26,8 @@ variables              = variables(1,:); %Strings with the names of the variable
 transformations        = numbers(1,:); %Numbers which represents which transformation to apply
 In_or_out              = numbers(2,:); %Zero: variable out of the system, one: variables inside
 nseries                = numbers(3,:); %Just the numner of the series (probably not useful)
-full_data              = numbers(5:end,:); %Start from 5 since we avoid a useless initial NaN
+position               = numbers(4,:); % the position of the variable in the VAR
+full_data              = numbers(6:end,:); %Start from 6 since we avoid a useless initial NaN
 
 nvar = size(variables,2);
 % Transformations
@@ -41,14 +54,14 @@ for i_var = 1:nvar
 end
 
 % Selecting the VAR system we want to work with
-i_system = 1;
+data     = zeros(size(full_data,1), sum(In_or_out));
+var_names= cell(1, sum(In_or_out));
 for i_var = 1:nvar
       if In_or_out(i_var) == 1
-            data(:,i_system) = full_data(:,i_var);
-            var_names(i_system) = variables(i_var);
-            i_system = i_system + 1;
+            data(:,position(i_var)) = full_data(:,i_var);
+            var_names(position(i_var)) = variables(i_var);
       elseif In_or_out(i_var) == 0
-            i_system = i_system;
+            % keep variable out of VAR
       else
             warning('The selection procedure of the system may be mispecified')
       end    
--- try_read_data2_file.m
+++ try_read_data2_file.m
@@ -4,4 +4,4 @@ close all
 filename = 'dataset_main';
 sheet    = 'Data';
 range    = 'B1:M286';
-[data2, variables] = read_data2(filename, sheet, range);
+[data2, variables] = read_da ta2(filename, sheet, range);
