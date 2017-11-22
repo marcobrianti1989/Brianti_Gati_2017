df3d7074d9eb9466d4fff4c7cd4d6b487adb280b
lauragati <lauraember@gmail.com>
2017-11-22 16:15:51 -0500

    Implemented automatic download for Michigan index and for Fernald's TFP

--- test_for_get_dataset_PC.m
+++ test_for_get_dataset_PC.m
@@ -5,10 +5,10 @@ clear
 close all
 
 tic % This takes about 50 sec.
-% get_dataset_test_pc; % this downloads all the FRED series (81 series) and save them to individual
-% % excel files
+get_dataset_test_pc; % this downloads all the FRED series (81 series) and save them to individual
+% excel files
 
-% Load in the SP500 series and convert them to quarterly
+%% Load in the SP500 series and convert them to quarterly
 % Note: the monthly series starts in Jan 1950, so the quarterly starts in
 % 1950-Q1.
 base_path = pwd;
@@ -31,9 +31,21 @@ comment = ['Date aggregated: ', date_aggregated];
 filename = [name_SP, starting_date_SP500];
 xlswrite(filename,SP500)
 
+
+%% Download Michigan index business conditions series as a CSV file in Single series
+
+url_mich_bus_csv = 'https://data.sca.isr.umich.edu/sda-public/tmpdir/AAc51CRa.csv'; % business conditions
+filename_mich = 'mich'; % this is the name of the CSV file
+outfilename = websave(filename_mich,url_mich_bus_csv);
+
+%% Download Fernald's TFP database
+
+url_fernald = 'https://www.frbsf.org/economic-research/files/quarterly_tfp.xlsx';
+web_tfp = webread(url_fernald);
+filename_fernald = 'fernald';
+outfilename = websave(filename_fernald,url_fernald);
+
+
 cd(base_path)
-
-
-
 toc
 
