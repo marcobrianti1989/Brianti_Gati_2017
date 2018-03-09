3167e7a6c6d09039cf753deaa894bcab2b2d905e
Marco Brianti <marcobrianti@live.it>
2018-03-09 10:37:01 -0500

    Adjusting some mistakes in the constraints
    Need to find a simple empirical exercise to check the correctness of each step. It would be useful to briefly replicate Beaudry and Portier maybe...

--- constraint_long.m
+++ constraint_long.m
@@ -6,8 +6,10 @@ function [c, ceq] = constraint_long(B,Xi,r,nvar)
 % Objective. Set to zero r column of the matrx Xi*B following Lutkepohl (2005)
 % The number of elements of Xi*B sets equal to zero is nvar*r
 c = [];
+ceq = zeros(nvar*r,1); % r*nvar is the number of constraints
 obj_matrix = Xi*B;
-obj_matrix(:,nvar-r:end)
-ceq = vertcat(obj_matrix(:,nvar-r:end)); % nvar*r is the number of constraints
+obj_matrix = obj_matrix(:);
+obj_matrix_target = obj_matrix(((nvar-r)*nvar)+1:end);
+ceq = obj_matrix_target; % nvar*r is the number of constraints
 
 end
--- structural_VECM.m
+++ structural_VECM.m
@@ -1,4 +1,4 @@
-function [B, Xi] = structural_VECM(alp,bet,Gam,res,sigma,nlags,r)
+function [B_opt, Xi] = structural_VECM(alp,bet,Gam,res,sigma,nlags,r)
 
 % INPUTS:
 % res is (T, nvar) - residuals from the reduced form
@@ -9,9 +9,17 @@ function [B, Xi] = structural_VECM(alp,bet,Gam,res,sigma,nlags,r)
 % alp is the loading matrix (nvar,r) where r is the number of temporary shocks
 
 % OUTPUT:
-% TBD
+% Obtain the impact matrix B (nvar,nvar). Interpretation of B is pretty
+% similar to the impact matrist of a SVAR. The procedure is more difficult
+% since we need just r(r-1)/2 constraints (impact restrictions) since r*(nvar - r)
+% independent restrictions have been already imposed with the reduced rank regression
+% from the reduced form VECM. I still slightly miss the intuition. But the
+% idea is that number of cointegrations is related to the number of
+% transitory shocks since it implies that in the long run, the effect of a
+% transitory shocks can be swiped out and it remains just the common trend.
 
-%Again I am using the notation of Lutkepohl (2005) - EUI, working paper
+% NOTICE
+% Again I am using the notation of Lutkepohl (2005) - EUI, working paper
 
 %Useful tools
 nvar = size(sigma,1);
@@ -27,14 +35,25 @@ for i = 1:nlags
 end
 Xi = bet*(alp'*G*bet)^(-1)*alp';
 
-
-obj = @(B) LL_VECM(T,B,sigma); %sum(sum(T/2*logm(B.^2) + T/2*trace((B')^(1)*B^(1)*sigma)));
+% Objective function to minimize - See Lutkepohl (2005) - EUI, working paper
+obj = @(B) LL_VECM(T,B,sigma); 
+%sum(sum(T/2*logm(B.^2) + T/2*trace((B')^(1)*B^(1)*sigma)));
 
 %Constraint that r(r-1)/2 elements of B must be zero.
-Me       = 1; %  Me = no. of equality constraints
+Me       = r*(r-1)/2; %  Me = no. of equality constraints. It must be r*(r - 1)/2 in a VECM
 Beq      = zeros(Me,1); % Beq is (Me x 1) where
 Aeq      = zeros(Me,nvar*nvar); % Aeq is (Me x (nvar*nvar)) - it is B vectorized
-Aeq(1,1) = 1; %zero-impact of news on TFP
+element_restricted = 1; %Coordinate to set the element equal to zero
+Aeq(1,element_restricted) = 1; %zero-impact restriction(s)
+
+% NOTICE. For some reasons which I am not fully understannding I can only
+% impose independent restrictions of the first column of B, i.e.
+% element_restricted can go from 1 to 3. If it is 4, 5, or 6 then the
+% second column of B is zero. If it is 7, 8, or 9 then the third column of B is
+% zero. My intuition is that it may be related to the long run
+% restrictions. The zeros in Xi*B may imply that setting a zero in a specific element of 
+% some column of B implies that all the column will be zero for
+% cointegration. In any case, I need more thoughts on it.
 
 %Optimization Parameters
 options  = optimset('fmincon');
@@ -42,8 +61,15 @@ options  = optimset(options, 'TolFun', 1e-9, 'display', 'none');
 
 %Minimization 
 B_zero = rand(nvar);
+%B_opt = fmincon(obj, B_zero,[],[],Aeq,Beq,[],[],[],options);
 B_opt = fmincon(obj, B_zero,[],[],Aeq,Beq,[],[],@(B) constraint_long(B,Xi,r,nvar),options);
 %fmincon(FUN,X0,A,B,Aeq,Beq,LB,UB,NONLCON,OPTIONS)
+
+% Temporary tools to visualize the correctness of restrictions
+obj_min = obj(B_opt)
+obj_zero = obj(B_zero)
+B_opt
+long_restr = Xi*B_opt
 
 
 
