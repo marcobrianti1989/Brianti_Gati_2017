ec4fedf148396cf34c70548ff4a78bcbe9c0ab07
lauragati <lauraember@gmail.com>
2018-03-28 13:15:03 -0400

    work together on model with hopefully REALLY correct stationarization

--- Code/oulton2010/model_exog_stat.m
+++ Code/oulton2010/model_exog_stat.m
@@ -27,8 +27,8 @@ function [fyn, fxn, fypn, fxpn] = model_exog_stat(param)
 bet      = param.bet;  %Never call anything beta...it's a matlab function
 a        = param.a; % share of standard capital
 b        = param.b; % share of IT capital
-gami     = param.gami; % growth rate of equipment productivity
-gamc     = param.gamc;    % growth rate of neutral technology
+biggami     = param.biggami; % growth rate of equipment productivity
+biggamc     = param.biggamc;    % growth rate of neutral technology
 di       = param.di; % depreciation rate of capital IT
 dc       = param.dc; % depreciation rate of capital standard
 chi      = param.chi; % preference parameter.
@@ -36,42 +36,42 @@ chi      = param.chi; % preference parameter.
 %Declare Needed Symbols
 syms KC KI KC1 KC2 KI1 KI2 % 6 vars
 syms KC_p KI_p KC1_p KC2_p KI1_p KI2_p 
-syms GAMC GAMI % 2 vars
-syms GAMC_p GAMI_p 
+syms BIGGAMC BIGGAMI % 2 vars
+syms BIGGAMC_p BIGGAMI_p 
 syms YC YI C IC IT W RC RI H H1 H2 % 11 vars
 syms YC_p YI_p C_p IC_p IT_p W_p RC_p RI_p H_p H1_p H2_p  
 syms P P_p % 1 var  % -> 20 vars
 
 %Declare X and Y vectors
-% KC KI GAMC GAMI
-X  = [KC KI GAMC GAMI]; % vector of state variables  
-XP = [KC_p KI_p GAMC_p GAMI_p]; % p signifies t+1 
+% KC KI BIGGAMC BIGGAMI
+X  = [KC KI BIGGAMC BIGGAMI]; % vector of state variables  
+XP = [KC_p KI_p BIGGAMC_p BIGGAMI_p]; % p signifies t+1 
 
 % YC YI C IC IT W RC RI H H1 H2 KC1 KC2 KI1 KI2 P
 Y  = [YC YI C IC IT W RC RI H H1 H2 KC1 KC2 KI1 KI2 P]; % vector of controls
 YP = [YC_p YI_p C_p IC_p IT_p W_p RC_p RI_p H_p H1_p H2_p KC1_p KC2_p KI1_p KI2_p P_p] ;
 
 % Model Equations (my notes p. 287):
-f(1)    = -YC + (1+GAMC)*H1^(1-a-b)*KC1^(a)*KI1^(b); %
-f(end+1)= -YI + (1+GAMI)*H2^(1-a-b)*KC2^(a)*KI2^(b); %
+f(1)    = -YC + BIGGAMC*H1^(1-a-b)*KC1^(a)*KI1^(b); 
+f(end+1)= -YI + BIGGAMI*H2^(1-a-b)*KC2^(a)*KI2^(b); %
 f(end+1)= -KC + KC1 + KC2; 
 f(end+1)= -KI + KI1 + KI2; 
 f(end+1)= -H + H1 + H2; 
-f(end+1)= -KC_p*((1+GAMC)^((1-b)/(1-a-b)) *(1+GAMI)^((b)/(1-a-b))) + (1-dc)*KC + IC;
-f(end+1)= -KI_p*((1+GAMC)^((a)/(1-a-b)) *(1+GAMI)^((1-a)/(1-a-b))) + (1-di)*KI + IT; 
+f(end+1)= -KC_p*(BIGGAMC^((1-b)/(1-a-b)) *BIGGAMI^((b)/(1-a-b))) + (1-dc)*KC + IC;
+f(end+1)= -KI_p*(BIGGAMC^((a)/(1-a-b)) *BIGGAMI^((1-a)/(1-a-b))) + (1-di)*KI + IT; 
 f(end+1)= -YC + C + IC;
 f(end+1)= -YI + IT;
 f(end+1)= -W/C + chi;
-f(end+1)= -1 + bet*C/C_p* ((1/(1+GAMC))^((1-b)/(1-a-b)) *(1/(1+GAMI))^((b)/(1-a-b)))  *(RC_p + 1-dc);
-f(end+1)= -1 + bet*C/C_p* ((1/(1+GAMC))^((a)/(1-a-b)) *(1/(1+GAMI))^((1-a)/(1-a-b)))  *(RI_p/P_p + 1-di); 
-f(end+1)= -W + (1-a-b)*(1+GAMC)*H1^(-a-b)*KC1^(a)*KI1^(b); 
-f(end+1)= -RC + a*(1+GAMC)*H1^(1-a-b)*KC1^(a-1)*KI1^(b);
-f(end+1)= -RI + b*(1+GAMC)*H1^(1-a-b)*KC1^(a)*KI1^(b-1);
-f(end+1)= -W + (1-a-b)*(1+GAMI)*H2^(-a-b)*KC2^(a)*KI2^(b)*P; 
-f(end+1)= -RC + a*(1+GAMI)*H2^(1-a-b)*KC2^(a-1)*KI2^(b)*P; 
-f(end+1)= -RI + b*(1+GAMI)*H2^(1-a-b)*KC2^(a)*KI2^(b-1)*P; 
-f(end+1)= log(GAMC_p/gamc) - .95*log(GAMC/gamc); %taken directly from Ryan's example code.
-f(end+1)= log(GAMI_p/gami) - .95*log(GAMI/gami); %taken directly from Ryan's example code.
+f(end+1)= -1 + bet*C/C_p* ((1/(BIGGAMC))^((1-b)/(1-a-b)) *(1/(BIGGAMI))^((b)/(1-a-b)))  *(RC_p + 1-dc);
+f(end+1)= -1 + bet*C/C_p* ((1/(BIGGAMC))^((1-b)/(1-a-b)) *(1/(BIGGAMI))^((b)/(1-a-b)))* BIGGAMC/BIGGAMI *(RI_p/P_p + 1-di); %
+f(end+1)= -W + (1-a-b)*BIGGAMC*H1^(-a-b)*KC1^(a)*KI1^(b); 
+f(end+1)= -RC + a*BIGGAMC*H1^(1-a-b)*KC1^(a-1)*KI1^(b);
+f(end+1)= -RI + b*BIGGAMC*H1^(1-a-b)*KC1^(a)*KI1^(b-1);
+f(end+1)= -W + (1-a-b)*BIGGAMI*H2^(-a-b)*KC2^(a)*KI2^(b)*P; 
+f(end+1)= -RC + a*BIGGAMI*H2^(1-a-b)*KC2^(a-1)*KI2^(b)*P; 
+f(end+1)= -RI + b*BIGGAMI*H2^(1-a-b)*KC2^(a)*KI2^(b-1)*P; 
+f(end+1)= log(BIGGAMC_p/biggamc) - .95*log(BIGGAMC/biggamc); %taken directly from Ryan's example code.
+f(end+1)= log(BIGGAMI_p/biggami) - .95*log(BIGGAMI/biggami); %taken directly from Ryan's example code.
 
 
 %Check Computation of Steady-State Numerically
--- Code/oulton2010/model_exog_stat_ss.m
+++ Code/oulton2010/model_exog_stat_ss.m
@@ -11,24 +11,24 @@ function [ss,param] = model_exog_stat_ss(param)
 bet      = param.bet;  %Never call anything beta...it's a matlab function
 a        = param.a; % share of standard capital
 b        = param.b; % share of IT capital
-gami     = param.gami; % growth rate of equipment productivity
-gamc     = param.gamc;    % growth rate of neutral technology
+biggami  = param.biggami; % growth rate of equipment productivity
+biggamc  = param.biggamc;    % growth rate of neutral technology
 di       = param.di; % depreciation rate of capital IT
 dc       = param.dc; % depreciation rate of capital standard
 chi      = param.chi; % preference parameter.
 
 %Use closed form expressions for the ss values. 
 % My notes p. 289-292
-p  = (1+gamc)/(1+gami); % p. 291
-rc = 1/bet * (1+gamc)^((1-b)/(1-a-b)) * (1+gami)^((b)/(1-a-b)) - (1-dc); % p. 289
-ri = (1/bet* (1+gamc)^((1-b)/(1-a-b)) *(1+gami)^((b)/(1-a-b)) -(1-di))*p; 
+p  = (biggamc)/(biggami); % p. 291
+rc = 1/bet * (biggamc)^((1-b)/(1-a-b)) * (biggami)^((b)/(1-a-b)) - (1-dc); % p. 289
+ri = (1/bet* (biggamc)^((1-b)/(1-a-b)) *(biggami)^((b)/(1-a-b)) -(1-di))*p; 
 
 % p. 292
-kc1_h1 = (a/rc)^((1-b)/(1-a-b)) * (1+gamc)^(1/(1-a-b)) * (b/ri)^(b/(1-a-b));%  eq.1 and p. 293 comment
-ki1_h1 = (b*(1+gamc)/ri)^(1/(1-b)) * kc1_h1^(a/(1-b)); % eq.2 and p. 293 comment
-w      = (1-a-b)*(1+gamc)*kc1_h1^a * ki1_h1^b; % eq.3 and p. 293 comment
-h1     = w/(chi*(1+gamc)*kc1_h1^a*ki1_h1^b - chi*kc1_h1*( ...
-    ((1+gamc)^((1-b)/(1-a-b)) *(1+gami)^((b)/(1-a-b))) - (1-dc))); %eq.4 and p. 293 comment
+kc1_h1 = (a/rc)^((1-b)/(1-a-b)) * (biggamc)^(1/(1-a-b)) * (b/ri)^(b/(1-a-b));%  eq.1 and p. 293 comment
+ki1_h1 = (b*(biggamc)/ri)^(1/(1-b)) * kc1_h1^(a/(1-b)); % eq.2 and p. 293 comment
+w      = (1-a-b)*(biggamc)*kc1_h1^a * ki1_h1^b; % eq.3 and p. 293 comment
+h1     = w/(chi*(biggamc)*kc1_h1^a*ki1_h1^b - chi*2*kc1_h1*( ...
+    ((biggamc)^((1-b)/(1-a-b)) *(biggami)^((b)/(1-a-b))) - (1-dc))); %eq.4 and p. 293 comment
 
 kc1 = kc1_h1*h1;
 ki1 = ki1_h1*h1;
@@ -42,14 +42,14 @@ ki = ki1+ki2;
 h  = h1+h2;
 
 c      = w/chi;
-ic     = (  ((1+gamc)^((1-b)/(1-a-b)) *(1+gami)^((b)/(1-a-b)))   -(1-dc))*kc; % p. 289
-it     = (  ((1+gamc)^((a)/(1-a-b)) *(1+gami)^((1-a)/(1-a-b)))   -(1-di))*ki;
+ic     = (  ((biggamc)^((1-b)/(1-a-b)) *(biggami)^((b)/(1-a-b)))   -(1-dc))*kc; % p. 289
+it     = (  ((biggamc)^((a)/(1-a-b)) *(biggami)^((1-a)/(1-a-b)))   -(1-di))*ki;
 yc     = c+ic;
 yi     = it;
 
 %Put the ss values in a vector consistent with Y and X vectors in model.m
 % KC KI GAMC GAMI
-xx  = [kc ki gamc gami]; 
+xx  = [kc ki biggamc biggami]; 
 yy  = [yc yi c ic it w rc ri h h1 h2 kc1 kc2 ki1 ki2 p];
 % YC YI C IC IT W RC RI H H1 H2 KC1 KC2 KI1 KI2 P
 
