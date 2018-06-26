6047c7acbceff01580bf2d6fe377273203ef1ae9
Marco Brianti <marcobrianti@live.it>
2018-06-26 11:48:01 -0400

    moire work

--- Code/just_IT_vecm/main_file_just_IT.m
+++ Code/just_IT_vecm/main_file_just_IT.m
@@ -64,7 +64,7 @@ which_variable  = which_shock; % select IT as the variable whose FEV we wanna ma
 %%Checking the number of lags over BIC, AIC, and HQ (see 'Lecture2M' in our folder)
 [AIC,BIC,HQ] = aic_bic_hq(data,max_lags);
 if AIC >= 4
-      nlags = 1; % 2;
+      nlags = 2; % 2;
       warning(['AIC > 4, setting nlags to ', num2str(nlags) ])
 else
       nlags = AIC;
